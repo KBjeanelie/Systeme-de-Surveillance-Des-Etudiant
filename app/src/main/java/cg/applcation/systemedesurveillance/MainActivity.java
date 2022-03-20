3aecc1d76f073353772a9b79f6efc674aac2e7da
kbjeanelie <jeaneliekubembula@gmail.com>
2022-03-20 15:13:34 +0100

    Ajout des bouton dans l'activité main pour pouvoir acceder au different interface creer dans la partie administrateur

--- app/src/main/java/cg/applcation/systemedesurveillance/MainActivity.java
+++ app/src/main/java/cg/applcation/systemedesurveillance/MainActivity.java
@@ -2,14 +2,48 @@ package cg.applcation.systemedesurveillance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import cg.applcation.systemedesurveillance.Administrateur.AddStudent;
+import cg.applcation.systemedesurveillance.Administrateur.AddTeacher;
+import cg.applcation.systemedesurveillance.Administrateur.AddUserAccount;
+import cg.applcation.systemedesurveillance.Administrateur.DisplayTeachers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int test = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddTeacher.class));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddStudent.class));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddUserAccount.class));
+            }
+        });
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DisplayTeachers.class));
+            }
+        });
     }
 }
